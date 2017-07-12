0970b269e50d31f4118e5eeaae612c6b22beef70
RobinBol <robinbolscher@gmail.com>
2017-07-12 15:46:32 +0200

    Nest Cam: add is_streaming Flow Card Condition

--- drivers/nest_cam/driver.js
+++ drivers/nest_cam/driver.js
@@ -1,11 +1,22 @@
 'use strict';
 
+const Homey = require('homey');
 const NestDriver = require('./../nestDriver');
 
 class NestCamDriver extends NestDriver {
 	onInit() {
 		super.onInit();
 		this.driverType = 'cameras';
+
+		new Homey.FlowCardCondition('is_streaming')
+			.on('run', (args, state, callback) => {
+				if (args && (args.hasOwnProperty('device'))) {
+					const device = args.device;
+					return callback(null, device.client.is_streaming);
+				}
+				return callback('invalid arguments provided');
+			})
+			.register();
 	}
 }
 
